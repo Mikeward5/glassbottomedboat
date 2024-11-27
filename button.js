dd4a0cd3220e728b457eec1bb6befa8907e06199
Mikeward5 <mikeward5@hotmail.co.uk>
2024-11-27 15:00:06 +0000

    readjusted links to middle

--- button.js
+++ button.js
@@ -1,4 +1,6 @@
 const myFunction = () => {
-  document.querySelector("#dropdown").style.display = "flex";
   document.querySelector("header").style.height = "140px";
+  document.querySelector("#dropdown").style.display = "flex";
+  document.querySelector("#dropdown").style.flexDirection = "column";
+  document.querySelector("#dropdown").style.alignItems = "center";
 };
