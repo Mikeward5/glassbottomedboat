23c39f8b86d83f99c526a27db1ec014d3c3381fe
Mikeward5 <mikeward5@hotmail.co.uk>
2024-12-20 16:34:09 +0000

    added ARIA header and footer

---
+++ info.js
@@ -0,0 +1,56 @@
+let buttonOne = document.getElementById("button1");
+let menuOne = document.getElementById("menu1");
+let buttonTwo = document.getElementById("button2");
+let menuTwo = document.getElementById("menu2");
+let buttonThree = document.getElementById("button3");
+let menuThree = document.getElementById("menu3");
+let buttonFour = document.getElementById("button4");
+let menuFour = document.getElementById("menu4");
+let buttonFive = document.getElementById("button5");
+let menuFive = document.getElementById("menu5");
+
+function openMenuOne() {
+  if (menu1.style.display === "block") {
+    menu1.style.display = "none";
+  } else {
+    menu1.style.display = "block";
+  }
+}
+
+function openMenuTwo() {
+  if (menu2.style.display === "block") {
+    menu2.style.display = "none";
+  } else {
+    menu2.style.display = "block";
+  }
+}
+
+function openMenuThree() {
+  if (menu3.style.display === "block") {
+    menu3.style.display = "none";
+  } else {
+    menu3.style.display = "block";
+  }
+}
+
+function openMenuFour() {
+  if (menu4.style.display === "block") {
+    menu4.style.display = "none";
+  } else {
+    menu4.style.display = "block";
+  }
+}
+
+function openMenuFive() {
+  if (menu5.style.display === "block") {
+    menu5.style.display = "none";
+  } else {
+    menu5.style.display = "block";
+  }
+}
+
+buttonOne.addEventListener("click", openMenuOne);
+buttonTwo.addEventListener("click", openMenuTwo);
+buttonThree.addEventListener("click", openMenuThree);
+buttonFour.addEventListener("click", openMenuFour);
+buttonFive.addEventListener("click", openMenuFive);
